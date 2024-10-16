dd5f1323b83e87a5f8bfbdeb0356802f5cd9fac1
Ruzan Pithawala <36655798+RuzanPithawala@users.noreply.github.com>
2024-10-16 16:40:54 -0400

    Create rightside.java

---
+++ rightside.java
@@ -0,0 +1,18 @@
+class Solution {
+    List<Integer> res;
+    public List<Integer> rightSideView(TreeNode root) {
+        res = new ArrayList<>();
+
+        dfs(root,0);
+        return res;
+    }
+    private void dfs(TreeNode root, int level){
+        if(root==null) return;
+
+        if(res.size()==level){
+            res.add(root.val);
+        }
+        dfs(root.right,level+1);
+        dfs(root.left, level+1);
+    }
+}
